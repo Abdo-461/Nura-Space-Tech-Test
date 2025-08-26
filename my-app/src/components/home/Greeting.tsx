87e481239ba118c4884f28410e6eab3ab1254d5a
Abdulrahman <abdulrahmanmvp93@gmail.com>
2025-08-26 21:26:00 +1000

    clean code

--- my-app/src/components/home/Greeting.tsx
+++ my-app/src/components/home/Greeting.tsx
@@ -9,9 +9,7 @@ export default function Greeting({ nameOne, nameTwo }: GreetingProps) {
   return (
     <div className="greeting-container">
       <h1>Welcome to NuraSpace, {nameOne} {nameTwo}!</h1>
-      <h2>Welcome to NuraSpace, but really…
-        you've entered Aby's headspace
-      </h2>
+      <h2>Welcome to NuraSpace, but really… you've entered Aby's headspace</h2>
       <h3>Population: clean code and bad puns 👨‍💻.</h3>
     </div>
   )
